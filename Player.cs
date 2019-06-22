dd05291f81f84b8fd79fe4ea2cd31af25762a632
denislav-i <50394677+denislav-i@users.noreply.github.com>
2019-06-22 16:13:00 +0300

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,40 @@
+﻿namespace NBAPlayers
+{
+    public class Player
+    {
+        private string name;
+        private int playingSince;
+        private string position;
+        private int rating;
+
+        public Player()
+        {
+
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int PlayingSince
+        {
+            get { return this.playingSince; }
+            set { this.playingSince = value; }
+        }
+
+        public string Position
+        {
+            get { return this.position; }
+            set { this.position = value; }
+        }
+
+        public int Rating
+        {
+            get { return this.rating; }
+            set { this.rating = value; }
+        }
+
+    }
+}
